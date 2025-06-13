8e61dd5b0d65b2049b1c80e6f5959d10ecb22645
FathonySyaennulloh <fathony404@gmail.com>
2025-06-13 13:25:50 +0700

    Progress Navbar

---
+++ src/services/Data.js
@@ -0,0 +1,27 @@
+export const NavbarMenu = [
+    {
+        id: 1,
+        title: "Home",
+        link:'/#'
+    },
+    {
+        id: 2,
+        title: "About me",
+        link:'/#'
+    },
+    {
+        id: 3,
+        title: "Mentors",
+        link:'/#'
+    },
+    {
+        id: 4,
+        title: "Career Test",
+        link:'/#'
+    },
+    {
+        id: 5,
+        title: "Blog",
+        link:'/#'
+    }
+]
