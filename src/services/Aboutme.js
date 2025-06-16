d29615293524cf2cac46183e67aab05d621b7f93
FathonySyaennulloh <fathony404@gmail.com>
2025-06-16 13:54:45 +0700

    add data careermate

---
+++ src/services/Aboutme.js
@@ -0,0 +1,8 @@
+const Aboutme = [
+    {
+        id:1,
+        Tittle : "Personalized Career Test",
+        caption: "Discover your interests, personality and career potential through tests developed with psychologists and HR professionals."
+
+    }
+]
