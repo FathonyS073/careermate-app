f1a78ba33c8c93e08178986fa84010397cc4a966
FathonySyaennulloh <fathony404@gmail.com>
2025-06-16 16:05:01 +0700

    Progress Company Section

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { LogoCompany } from "../services/Company";
 
 const Company = () => {
-  return <div className="w-full bg-[#4A90E2] h-[150px]"></div>;
+  return (
+    <div className="w-full bg-[#4A90E2] container h-[205px] pt-10">
+      <div className="flex justify-between items-center">
+        {LogoCompany.map((item) => {
+          return (
+            <img
+              key={item.id}
+              src={item.logo}
+              className="w-[80px] md:w-[100px] h-[40px]"
+              alt=""
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Company;
---
+++ src/services/Company.js
@@ -0,0 +1,32 @@
+import Amazon from "../assets/Images/Amazon.png";
+import Walmart from "../assets/Images/Walmart.png";
+import Deloitte from "../assets/Images/Deloitte.png";
+import Uber from "../assets/Images/Uber.png";
+import Google from "../assets/Images/Google.png";
+
+export const LogoCompany = [
+    {
+        id:1,
+        logo:Amazon,
+        width:"169px",
+        height:"51px"
+    },
+    {
+        id:2,
+        logo:Walmart,
+        width:"215px",
+        height:"51px"
+    },
+    {
+        id:3,
+        logo:Deloitte
+    },
+    {
+        id:4,
+        logo:Uber
+    },
+    {
+        id:5,
+        logo:Google
+    }
+]
