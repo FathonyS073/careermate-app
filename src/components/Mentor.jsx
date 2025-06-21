4ac7aa6b96a9c184e1bf6d42a9478015774756a4
FathonySyaennulloh <fathony404@gmail.com>
2025-06-21 22:43:11 +0700

    Mentor Section Done

--- src/components/Mentor.jsx
+++ src/components/Mentor.jsx
@@ -5,8 +5,8 @@ import Image1 from "../assets/Images/1.png";
 
 const Mentor = () => {
   return (
-    <div className="bg-backgroundPrimer container py-14">
-      <div className="flex items-center justify-center flex-col gap-3">
+    <div className="bg-backgroundPrimer py-14">
+      <div className="container flex items-center justify-center flex-col gap-3">
         {/* Subtittle */}
         <SubTitle title="Mentor CareerMate" />
         {/* Tittle and Desk */}
@@ -20,20 +20,33 @@ const Mentor = () => {
         </p>
       </div>
       {/* Image Mentor */}
-      <div className="flex flex-row my-10">
-        <div className="card bg-mentor  w-[369px] h-[288px] items-center flex justify-center">
-          {/* Image */}
-          <img src={Image1} alt="" width="203px" height="295px" className="" />
-          {/* Name & Position*/}
-          <div className="absolute bg-cardmentor py-3 pr-14 pl-2 mt-36 rounded-[5px] border-[#C5DCF6] border-2 flex flex-col gap-2">
-            <h1 className="text-title font-medium text-[20px]">
-              Olivia Christin
-            </h1>
-            <p className="text-desk font-[20px]">
-              Specialized Digital Marketing
-            </p>
-          </div>
-        </div>
+      <div className="flex flex-row my-10 w-full gap-3  ">
+        {MentorImage.map((item) => {
+          return (
+            <div
+              className="card bg-mentor  w-[369px] h-[288px] items-center flex justify-center "
+              key={item.id}
+            >
+              {/* Image */}
+              <div className="w-[203px] h-[288px] flex items-end">
+                <img
+                  src={item.img}
+                  alt=""
+                  width="203px"
+                  height="295px"
+                  className="filter grayscale hover:grayscale-0"
+                />
+              </div>
+              {/* Name & Position*/}
+              <div className="absolute bg-cardmentor w-[246px] h-[90px] mt-36 pl-3 rounded-[5px] border-[#C5DCF6] border-2 flex flex-col gap-1 justify-center overflow-x-hidden scrollbar-hide scroll-smooth">
+                <h1 className="text-title font-medium text-[20px]">
+                  {item.name}
+                </h1>
+                <p className="text-desk font-[20px]">{item.position}</p>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/services/MentorImage.js
+++ src/services/MentorImage.js
@@ -1,38 +1,42 @@
-import Mentor1 from "../assets/Images/1.png";
-import Mentor2 from "../assets/Images/2.png";
-import Mentor3 from "../assets/Images/3.png";
+import Mentor3 from "../assets/Images/1.png";
+import Mentor2 from "../assets/Images/5.png";
+import Mentor1 from "../assets/Images/2.png";
+import Mentor5 from "../assets/Images/3.png";
 import Mentor4 from "../assets/Images/4.png";
-import Mentor5 from "../assets/Images/5.png";
-
 export const MentorImage = [
   {
     id: 1,
-    name: "Olivia Christin",
+    name: "Jhon Doe",
     img: Mentor1,
-    position:'Specialized Digital Marketing'
+    position:'Specialized Digital Marketing',
+    width:"206px",
   },
   {
     id: 2,
-    name: "jhon Doe",
+    name: "Fieza Naura",
     img: Mentor2,
-    position:'Specialized Digital Marketing'
+    position:'Digital Content Deloitte',
+    width:"200px",
   },
   {
     id: 3,
-    name: "Olivia",
+    name: "Olivia Christin",
     img: Mentor3,
-    position:'Specialized Digital Marketing'
+    position:'Specialized Digital Marketing',
+    width:"206px",
   },
   {
     id: 4,
     name: "Sie chi Khan",
     img: Mentor4,
-    position:'Lead IT Security Google'
+    position:'Lead IT Security Google',
+    width:"206px",
   },
   {
     id: 5,
-    name: "Naurah Aprilia",
+    name: "Olivia",
     img: Mentor5,
-    position:'Lead Hr Deloitte'
+    position:'Lead Hr Deloitte',
+    width:"206px",
   },
 ];
