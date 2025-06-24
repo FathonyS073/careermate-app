a25ec2a84bf164a70be2a7360d13149c3d38e4fe
FathonySyaennulloh <fathony404@gmail.com>
2025-06-24 15:02:26 +0700

    Prepare Make : Testimonials Section

---
+++ src/services/DataTestimoni.js
@@ -0,0 +1,9 @@
+export const DataTesti = [
+    {
+        id:1,
+        testimoni:'"CareerMate opened my eyes to career paths I never imagined. The mentorship sessions truly changed my mindset!"',
+        name:'Daniel Thompson',
+        position:'Final Year Student',
+        type:'long',
+    }
+]
